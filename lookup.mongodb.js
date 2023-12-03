745cb4d3ebb99c702d2a8d24aefc8c43c6a45bce
Subin <sub_1n@yahoo.com>
2023-12-03 14:25:56 +0200

    lookup mongodb

---
+++ lookup.mongodb.js
@@ -0,0 +1,68 @@
+use("MERN_DB");
+
+// db.courses.insertMany([
+//   {
+//     name: "MERN",
+//     duration: "3 months",
+//     price: 25000,
+//   },
+//   {
+//     name: "Python with Data science",
+//     duration: "2.5 months",
+//     price: 22000,
+//   },
+//   {
+//     name: "Cyber security",
+//     duration: "3 months",
+//     price: 35000,
+//   },
+//   {
+//     name: "Devops engineer",
+//     duration: "3 months",
+//     price: 45000,
+//   },
+// ]);
+
+// db.learners.insertMany([
+//   {
+//     name: "Sujit Sharma",
+//     address: "Kalanki",
+//     phone: "984612356",
+//     course: ObjectId("656c60bd744bc873c20ec6ec"),
+//   },
+//   {
+//     name: "Sahil Khatiwoda",
+//     address: "Gwarkho",
+//     phone: "9846123785",
+//     course: ObjectId("656c60bd744bc873c20ec6ed"),
+//   },
+// ]);
+
+db.learners.aggregate([
+  {
+    $match: {
+      name: "Sujit Sharma",
+    },
+  },
+  {
+    $lookup: {
+      from: "courses",
+      localField: "course",
+      foreignField: "_id",
+      as: "courseData",
+    },
+  },
+  {
+    $project: {
+      name: 1,
+      address: 1,
+      phone: 1,
+      _id: 0,
+      courseDetails: {
+        name: { $first: "$courseData.name" },
+        price: { $first: "$courseData.price" },
+        duration: { $first: "$courseData.duration" },
+      },
+    },
+  },
+]);
